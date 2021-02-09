d618fe29a33c0fd7c4707563c6d7278471f4df5b
pkabus <peter.kabus@gmx.de>
2021-02-09 12:41:27 +0100

    DEV-003 Add class and functional components with props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Greet from './components/Greet'
+import Welcome from './components/Welcome'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Greet name="Bruce" heroName="Batman" >
+        <p>This is children props</p>
+      </Greet>
+      <Greet name="Diana" heroName="Wonder Woman" >
+        <button>Action</button>
+      </Greet>
+      <Greet name="Clark" heroName="Superman" />
+
+      <Welcome name="Bruce" heroName="Batman"></Welcome>
+      <Welcome name="Diana" heroName="Wonder Woman" ></Welcome>
+      <Welcome name="Clark" heroName="Superman"></Welcome>
     </div>
   );
 }
