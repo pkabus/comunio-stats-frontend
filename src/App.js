4c6f3e27a7ae44806a8a19b1e2ce3f05353f3685
pkabus <peter.kabus@gmx.de>
2021-02-10 09:51:53 +0100

    DEV-003 Methods as props

--- src/App.js
+++ src/App.js
@@ -7,18 +7,20 @@ import Counter from './components/Counter'
 import FunctionClick from './components/FunctionClick'
 import ClassClick from './components/ClassClick'
 import EventBind from './components/EventBind'
+import ParentComponent from './components/ParentComponent'
 
 function App() {
   return (
     <div className="App">
-      <Greet name="Diana" heroName="Wonder Woman" >
-        <button>Action</button>
-      </Greet>
-      <Welcome name="Bruce" heroName="Batman"></Welcome>
-      <Counter></Counter>
-      <FunctionClick />
-      <ClassClick />
-      <EventBind />
+      <ParentComponent />
+      {/* <Greet name="Diana" heroName="Wonder Woman" > */}
+        {/* <button>Action</button> */}
+       {/* </Greet> */}
+      {/* <Welcome name="Bruce" heroName="Batman"></Welcome> */}
+      {/* <Counter></Counter> */}
+      {/* <FunctionClick /> */}
+      {/* <ClassClick /> */}
+      {/* <EventBind /> */}
     </div>
   );
 }
