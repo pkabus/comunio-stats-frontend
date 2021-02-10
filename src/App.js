808c05d448e8135bc29ef071be42c430953816ff
pkabus <peter.kabus@gmx.de>
2021-02-10 16:29:21 +0100

    DEV-003 ReactJS Tutorial 23 - Component Mounting Lifecycle Methods

--- src/App.js
+++ src/App.js
@@ -11,11 +11,13 @@ import ParentComponent from './components/ParentComponent'
 import UserGreeting from './components/UserGreeting';
 import NameList from './components/NameList';
 import Form from './components/Form';
+import LifecycleA from './components/LifecycleA';
 
 function App() {
   return (
     <div className="App">
-      <Form />
+      <LifecycleA />
+      {/* <Form /> */}
       {/* <NameList /> */}
       {/* <UserGreeting /> */}
       {/* <ParentComponent /> */}
