a887a2a9e821d1f1056ee74ebdd649b19655740d
pkabus <peter.kabus@gmx.de>
2021-02-10 18:06:25 +0100

    DEV-003 ReactJS Tutorial 26 - Pure Components

--- src/App.js
+++ src/App.js
@@ -14,11 +14,14 @@ import Form from './components/Form';
 import LifecycleA from './components/LifecycleA';
 import FragmentDemo from './components/FragmentDemo';
 import Table from './components/Table';
+import PureComp from './components/PureComp';
+import ParentComp from './components/ParentComp';
 
 function App() {
   return (
     <div className="App">
-      <Table />
+      <ParentComp />
+      {/* <Table /> */}
       {/* <FragmentDemo /> */}
       {/* <LifecycleA /> */}
       {/* <Form /> */}
