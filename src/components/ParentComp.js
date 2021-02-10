5124ca7e718553953319f0ea4f32bb488cdaa800
pkabus <peter.kabus@gmx.de>
2021-02-10 18:17:31 +0100

    DEV-003 ReactJS Tutorial 27 - Memo Components

--- src/components/ParentComp.js
+++ src/components/ParentComp.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import MemoComp from './MemoComp'
 import PureComp from './PureComp'
 import RegComp from './RegComp'
 
@@ -25,8 +26,9 @@ class ParentComp extends Component {
         return (
             <div>
                 Parent Component
-                <RegComp name={this.state.name}></RegComp>
-                <PureComp name={this.state.name}></PureComp>
+                <MemoComp name={this.state.name} />
+                {/* <RegComp name={this.state.name}></RegComp> */}
+                {/* <PureComp name={this.state.name}></PureComp> */}
             </div>
         )
     }
