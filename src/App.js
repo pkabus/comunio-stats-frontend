7783b651eda487a192c76b6212bcd39f0e351275
pkabus <peter.kabus@gmx.de>
2021-02-11 15:43:20 +0100

    DEV-003 ReactJS Tutorial 39 - Context Part 1 and 2

--- src/App.js
+++ src/App.js
@@ -5,16 +5,21 @@ import HoverCounter from './components/HoverCounter';
 import ClickCounterTwo from './components/ClickCounterTwo';
 import HoverCounterTwo from './components/HoverCounterTwo';
 import Counter from './components/Counter';
+import ComponentC from './components/ComponentC';
+import { UserProvider } from './components/userContext';
 
 function App() {
   return (
     <div className="App">
-      <Counter render={(count, incrementCount) => (
+      <UserProvider value="Vishwas">
+        <ComponentC />
+      </UserProvider>
+      {/* <Counter render={(count, incrementCount) => (
         <ClickCounterTwo count={count} incrementCount={incrementCount} />)}
       />
       <Counter render={(count, incrementCount) => (
         <HoverCounterTwo count={count} incrementCount={incrementCount} />)}
-      />
+      /> */}
       {/* <HoverCounterTwo /> */}
       {/* <ClickCounterTwo /> */}
       {/* <HoverCounter /> */}
