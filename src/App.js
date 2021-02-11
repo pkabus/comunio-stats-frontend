36e7b73e8385da1a87813585c2c09fb021f5617c
pkabus <peter.kabus@gmx.de>
2021-02-11 10:58:26 +0100

    DEV-003 ReactJS Tutorial 34 - Higher Order Components Part 1 and 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import ClickCounter from './components/ClickCounter';
+import HoverCounter from './components/HoverCounter';
 
 function App() {
   return (
     <div className="App">
-      
+      <HoverCounter />
+      <ClickCounter />
     </div>
   );
 }
