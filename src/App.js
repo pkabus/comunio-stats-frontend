7b30d0ed58671bf99caa30bfee132fbe0d39700f
pkabus <peter.kabus@gmx.de>
2021-02-11 09:49:23 +0100

    DEV-003 Clean up from previous tutorial lessons

--- src/App.js
+++ src/App.js
@@ -1,62 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-
-import Greet from './components/Greet'
-import Welcome from './components/Welcome'
-import Counter from './components/Counter'
-import FunctionClick from './components/FunctionClick'
-import ClassClick from './components/ClassClick'
-import EventBind from './components/EventBind'
-import ParentComponent from './components/ParentComponent'
-import UserGreeting from './components/UserGreeting';
-import NameList from './components/NameList';
-import Form from './components/Form';
-import LifecycleA from './components/LifecycleA';
-import FragmentDemo from './components/FragmentDemo';
-import Table from './components/Table';
-import PureComp from './components/PureComp';
-import ParentComp from './components/ParentComp';
-import RefsDemo from './components/RefsDemo';
-import FocusInput from './components/FocusInput';
-import FRParentInput from './components/FRParentInput';
-import PortalDemo from './components/PortalDemo';
-import Hero from './components/Hero';
-import ErrorBoundary from './components/ErrorBoundary';
+import ClickCounter from './components/ClickCounter';
 
 function App() {
   return (
     <div className="App">
-      <ErrorBoundary>
-        <Hero heroName="Batman" />
-      </ErrorBoundary>
-
-      <ErrorBoundary>
-        <Hero heroName="Superman" />
-      </ErrorBoundary>
-
-      <ErrorBoundary>
-        <Hero heroName="Joker" />
-      </ErrorBoundary>
-      {/* <PortalDemo /> */}
-      {/* <FRParentInput /> */}
-      {/* <FocusInput /> */}
-      {/* <RefsDemo /> */}
-      {/* <ParentComp /> */}
-      {/* <Table /> */}
-      {/* <FragmentDemo /> */}
-      {/* <LifecycleA /> */}
-      {/* <Form /> */}
-      {/* <NameList /> */}
-      {/* <UserGreeting /> */}
-      {/* <ParentComponent /> */}
-      {/* <Greet name="Diana" heroName="Wonder Woman" > */}
-      {/* <button>Action</button> */}
-      {/* </Greet> */}
-      {/* <Welcome name="Bruce" heroName="Batman"></Welcome> */}
-      {/* <Counter></Counter> */}
-      {/* <FunctionClick /> */}
-      {/* <ClassClick /> */}
-      {/* <EventBind /> */}
+      
     </div>
   );
 }
